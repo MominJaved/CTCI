083b22980dec76d78f1aae85629f8d1e9b1279da
Momin Javed <punjoo.mj@gmail.com>
2014-12-02 12:51:43 -0500

    added Compress2.java

--- chapter1/Compress2.java
+++ chapter1/Compress2.java
@@ -1,11 +1,13 @@
 public class Compress2 {
-	
+
 	public static String compressString(String str) {
+		if (compLength(str) > str.length())
+			return str;
 		int count = 1;
 		char prev = str.charAt(0);
 		StringBuffer buffer = new StringBuffer();
 	
-		for (int i = 1; i<str.length(); i++) {
+		for (int i = 1; i < str.length(); i++) {
 			if (str.charAt(i) == prev) {
 				count++;
 			}
@@ -22,7 +24,28 @@ public class Compress2 {
 		return comp;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(compressString("aabcccccaaa"));
+	public static int compLength(String string) {
+		if (string == null || string.isEmpty())
+			return 0;
+		char last = string.charAt(0);
+		int count = 1;
+		int len = 0;
+		for (int j = 1; j <string.length(); j++) {
+			if (string.charAt(j) == last) {
+				count++;
+			}
+			else {
+				len += 1 + String.valueOf(count).length();
+				last = string.charAt(j);
+				count = 1;
+			}
+		}
+		len += 1 + String.valueOf(count).length();
+		//System.out.println(len);
+		return len;
 	}
+
+	public static void main(String[] args) {
+                System.out.println(compressString("abcdefg"));
+        } 
 }
